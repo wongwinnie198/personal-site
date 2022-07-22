2c4e2294ae05e0ee0f291298b3b77f7a823da002
wongwinnie198 <wongwinnie198@gmail.com>
2022-07-22 15:14:40 +0800

    rewrite render method from returning str to generating using js

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -1,26 +1,31 @@
-const jumbotronData = {};
+const jumbotronData = [{}];
 class Jumbotron {
   constructor(props) {
     this.state = props;
   }
-
+  /**
+   * render()
+   * @returns an array of HTMLdivElements to be looped in
+   */
   render() {
-    return `
-      <div class="web-h1">
-        <h1><web></h1>
-        </div>
-        <div class="person-img">
-        <img src="">
-        </div>
-        <div class="coder-h1">
-        <h1>developer</h1>
-      </div>
-    `;
+    const [webHeadingDiv, personImgDiv, coderHeadingDiv] = [
+      document.createElement("div"),
+      document.createElement("div"),
+      document.createElement("div"),
+    ];
+    Object.assign(webHeadingDiv, { className: "web-heading" });
+    webHeadingDiv.innerHTML = `<h1><web></h1>`;
+    Object.assign(personImgDiv, { className: "person-img" });
+    personImgDiv.innerHTML = `<img src="">`;
+    Object.assign(coderHeadingDiv, { className: "coder-heading" });
+    coderHeadingDiv.innerHTML = `<h1>juumbo</h1>`;
+    return [webHeadingDiv, personImgDiv, coderHeadingDiv];
   }
 }
 
 let jumbotronComponent = new Jumbotron();
 document.addEventListener("DOMContentLoaded", function () {
-  console.log(document.querySelector(".jumbotron"));
-  document.querySelector(".jumbotron").innerHTML = jumbotronComponent.render();
+  jumbotronComponent
+    .render()
+    .forEach((item) => document.querySelector(".jumbotron").appendChild(item));
 });
